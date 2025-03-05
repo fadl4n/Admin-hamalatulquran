98e9b0fc8af56ebfad95e1a7b11ebca5a93bcfc3
fadl4n <fadlanmuhammad4444@gmail.com>
2025-03-05 16:52:03 +0700

    perbaikan sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,30 +1,21 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4"
-    style="height: 100vh; position: fixed; overflow-y: auto; background: #343a40; width: 250px;">
-
+    style="height: 100vh; overflow-y: scroll; background: #343a40; width: 250px; position: fixed; top: 0; left: 0; bottom: 0; scrollbar-width: none;">
 
     <!-- Brand Logo -->
     <a href="{{url('/')}}" class="brand-link text-center">
-      {{-- <img src="{{ asset ("/bower_components/admin-lte/dist/img/AdminLTELogo.png") }}" alt="AdminLTE Logo"
-        class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
       <span class="brand-text font-weight-bold text-center text-white">CMS</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-            with font-awesome or any other icon font library -->
           <li class="nav-item">
             <a href="{{url('/')}}" class="{{ (request()->is('/')) ? 'nav-link active' : 'nav-link' }}">
               <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-              </p>
+              <p>Dashboard</p>
             </a>
           </li>
           @if (isset($sidebarMenus))
@@ -50,10 +41,19 @@
               </li>
             @endforeach
           @endif
-
         </ul>
       </nav>
-      <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
-  </aside>
+</aside>
+
+<style>
+/* Hilangkan scrollbar di Chrome, Safari */
+.main-sidebar::-webkit-scrollbar {
+    display: none;
+}
+
+/* Hilangkan scrollbar di Firefox */
+.main-sidebar {
+    scrollbar-width: none;
+}
+</style>
